faf1904c3f079fecc7a4f93a60790aee60f3159f
Chinmay Thakur <thakurchinmay10@gmail.com>
2024-05-05 18:09:04 +0530

    Added isAbove18 function

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 
-const name = 'Chinmay Thakur'
+const name1 = 'Chinmay Thakur'
 const dob = '10/02/2004'
 
 function sayName() {
-    console.log('My name is' + name)
+    console.log('My name is' + name1)
+}
+
+function isAbove18(){
+    return age >= 18
 }
